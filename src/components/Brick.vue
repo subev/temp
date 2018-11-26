36e410353ab433f4ab9181a58c20ebceffba6dfa
Petur Subev <petur.subev@gmail.com>
2018-11-26 02:55:58 +0200

    Add pipes

--- src/components/Brick.vue
+++ src/components/Brick.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class='wrapper'>
+  <div class='wrapper' :id='id'>
      <div class="head">
        <div class='phone'/>
      </div>
      <div class="body">
        <slot name='default'></slot>
      </div>
+     <template v-if='id'>
+       <div id='draw-layer'>
+       </div>
+     </template>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component({
 })
 export default class Brick extends Vue {
-
+  @Prop()
+  id: string;
 }
 </script>
 <style lang="scss">
@@ -34,16 +39,26 @@ export default class Brick extends Vue {
   min-width: 100%;
 }
 
+#mainBrick {
+  position: relative;
+
+  #draw-layer {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+}
+
 .hello {
   overflow: scroll;
   height: 100%;
 }
 
 .wrapper {
-  border-width: 3px  3px 0px  3px;
   padding: 5px;
-  border-style:solid solid solid solid;
-  border-color: #00000010;
+  // border-width: 3px  3px 0px  3px;
+  // border-style:solid solid solid solid;
+  // border-color: #00000010;
   display: inline-flex;
   flex: 0 1 100%;
   flex-direction: column;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,45 @@
 <template>
   <div class="viewport">
     <div class="hello" tabindex="-1" @keydown.up="inc" @keydown.down="dec">
-      <Brick class='0' :style="this.style">
-        <Brick class='1'>
-        </Brick>
-        <Brick class='1'>
-          <Brick/>
-          <Brick/>
-          <Brick>
+      <div class='scaler' :style="this.style">
+        <Brick class='0' id="mainBrick">
+          <Brick class='1'>
+          </Brick>
+          <Brick class='1'>
+            <Brick/>
+            <Brick/>
             <Brick>
               <Brick>
                 <Brick>
-                <Brick>
-                  <Brick/>
-                </Brick>
+                  <Brick>
+                  <Brick>
+                    <Brick/>
+                  </Brick>
+                  </Brick>
                 </Brick>
               </Brick>
             </Brick>
+            <Brick/>
           </Brick>
-          <Brick/>
         </Brick>
-      </Brick>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import Brick from '@/components/Brick.vue'; // @ is an alias to /src
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Brick from "@/components/Brick.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    Brick,
-  },
+    Brick
+  }
 })
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 
-  scale: number = .2;
+  scale: number = 1;
 
   inc() {
     this.scale += 0.1;
@@ -47,29 +49,111 @@ export default class HelloWorld extends Vue {
     this.scale -= 0.1;
   }
 
+  mounted(): void {
+    this.initializeDiagram();
+  }
+  updated(): void {
+
+  }
+
+  initializeDiagram(): void {
+    var $ = go.GraphObject.make;
+    var diagram = $(
+      go.Diagram,
+      "draw-layer",
+      {
+        "isEnabled": false, //this disables all the below and even more
+        "animationManager.isEnabled": false,
+        "initialPosition": go.Point.parse("0 0")
+      }
+    );
+
+    window.diagram = diagram;
+
+    diagram.nodeTemplate = $(
+      go.Node,
+      "Auto",
+      { desiredSize: new go.Size(300, 100) },
+      new go.Binding("location", "loc", go.Point.parse),
+      // $(go.Shape, "RoundedRectangle", { fill: "lightgray" }),
+      $(
+        go.TextBlock,
+        // { margin: 15 }
+        //new go.Binding("text", "key")
+      )
+    );
+
+    diagram.linkTemplate = $(
+      go.Link,
+      {
+        routing: go.Link.AvoidsNodes,
+        corner: 10,
+        fromSpot: go.Spot.BottomSide,
+        toSpot: go.Spot.TopSide
+      },
+      $(
+        go.Shape, { strokeWidth: 4, stroke: "rgba(173, 173, 173, 0.25)" },
+        new go.Binding("strokeWidth", "width")
+      )
+    );
+
+    function getOffset(el) {
+      const rect = el.getBoundingClientRect();
+      return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+      };
+    }
+
+    const elements = [...this.$el.querySelectorAll('.phone')];
+    const viewport = this.$el.querySelectorAll('#draw-layer')[0];
+    const viewPortOffset = getOffset(viewport);
+
+    var nodeDataArray = elements.map((el, index) => {
+      const elOffset = getOffset(el);
+
+      return {
+        key: index,
+        loc: `${elOffset.left - viewPortOffset.left} ${elOffset.top - viewPortOffset.top}`,
+      }
+    })
+
+    var linkDataArray = [
+      { from: "0", to: "1", width: 30 },
+      { from: "0", to: "2", width: 50 },
+      { from: "2", to: "3", width: 1 },
+      { from: "2", to: "4", width: 5 },
+      { from: "2", to: "5", width: 40 },
+      { from: "2", to: "11", width: 10 },
+      { from: "5", to: "6", width: 40},
+      { from: "6", to: "7", width: 30},
+      { from: "7", to: "8", width: 20},
+      { from: "8", to: "9", width: 10},
+      { from: "9", to: "10", width: 5 },
+    ];
+    diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+  }
 
   get style() {
     return {
       transform: `scale(${this.scale})`
-    }
+    };
   }
-
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-html, body {
+html,
+body {
 }
 
 .hello {
-
 }
-
 
 .viewport {
   border: 3px solid black;
-  height: 1000px;
+  height: calc(100vh - 100px);
   width: 1000px;
   margin: auto;
 }
